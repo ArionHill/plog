313d324b1c4412ac2674d39cc794de3db3d94ab7
Arion <arionmail@163.com>
2026-01-16 17:47:52 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp23}
+CXX=${CXX:-/grid/common/pkgs/gcc/v15.2.0/bin/g++}
+C=${C:-/grid/common/pkgs/gcc/v15.2.0/bin/gcc}
+
+ln -sf $BUILD_DIR/$BUILD_TYPE-cpp23/compile_commands.json
+
+mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp23 \
+    && cd $BUILD_DIR/$BUILD_TYPE-cpp23 \
+    && cmake \
+           -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+           -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+           -DCMAKE_C_COMPILER=$C \
+           -DCMAKE_CXX_COMPILER=$CXX \
+           -DLIBSTDCPP_DIR=/grid/common/pkgsData/gcc-v15.2.0/Linux/RHEL8.0-2019-x86_64/lib64 \
+           -DLIBSTDCPP_DIR=/grid/common/pkgsData/gcc-v15.2.0/Linux/RHEL8.0-2019-x86_64/lib64 \
+           $SOURCE_DIR \
+    && make $*
+
+# Use the following command to run all the unit tests
+# at the dir $BUILD_DIR/$BUILD_TYPE :
+# CTEST_OUTPUT_ON_FAILURE=TRUE make test
+
+# cd $SOURCE_DIR && doxygen
